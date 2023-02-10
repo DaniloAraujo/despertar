a3c25a119d2ac89072102393adbed091a50948f2
Danilo Araujo <daniloo.ag2@gmail.com>
2023-02-10 10:12:24 -0300

    atualizacao

--- scripts/data.js
+++ scripts/data.js
@@ -1,22 +1,13 @@
 const modal = document.querySelector('.horarioTransmissao');
 const modalTitulo = document.querySelector('.tituloTransmissao');
-// const botao = document.querySelector('#btnCulto');
 const data = new Date()
 const dia = data.getDay()
 const hora = data.getHours()
 
-// botao.setAttribute('disabled', '')
 
-if (((dia === 0 && hora >= 10 && hora <= 12) || (dia === 3 && hora >= 19 && hora <= 22))) {
-    // botao.removeAttribute('disabled')
+if (((dia === 0 && hora >= 9 && hora <= 12) || (dia === 3 && hora >= 19 && hora <= 22))) {
 
-    modal.innerHTML += `<div class="ratio ratio-16x9">
-    <iframe src="https://www.youtube.com/embed/KsTnUurMR6A"
-        title="YouTube video player" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen></iframe>"
-        title="YouTube video" allowfullscreen></iframe>
-            </div>`;
+    modal.innerHTML += `<a href="https://www.youtube.com/channel/UCDLjvM9tEdjod3PdgFv8mnw">Igreja Despertar</a>`;
 
     modalTitulo.innerHTML += `<strong>Live Streaming...</strong>`;
 } else {
@@ -30,3 +21,19 @@ if (((dia === 0 && hora >= 10 && hora <= 12) || (dia === 3 && hora >= 19 && hora
 
     modalTitulo.innerHTML += `<strong>Sem live no momento.</strong>`;
 }
+
+
+/*
+Para quando for transmitir ao vivo.
+`<div class="ratio ratio-16x9">
+    <iframe src="https://www.youtube.com/channel/UCDLjvM9tEdjod3PdgFv8mnw"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>"
+        title="YouTube video" allowfullscreen></iframe>
+        </div>
+        <br>
+        <hr>
+        Informação: Nossas lives são liberadas 10 minutos antes de iniciar o culto.`;
+
+*/
